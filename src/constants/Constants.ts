c725a39882f0e0591415d9e3f77cddf57138a21b
Yevhenii Shevchenko <supershevchenko548@gmail.com>
2022-08-20 18:59:45 +0200

    feat: add main page by ts

---
+++ src/constants/Constants.ts
@@ -0,0 +1,17 @@
+export default class Constants {
+  static mainPage = {
+    headLine: 'RSLang - приложение для изучения английского',
+    paragraph: 'Позволяет выучить английский и др описание возможностей и преимуществ приложения строк на пять и др описание возможностей и преимуществ приложения строк на пять',
+    buttonStart: 'Начать',
+  };
+
+  static signIn = {};
+
+  static gamesPage = {};
+
+  static statisticPage = {};
+
+  static teamPage = {};
+
+  static textBookPage = {};
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import './assets/css/style.css';
 
 import openSignIn from './pages/signIn/openSignIn';
+import openMainPage from './pages/mainPage/openMainPage';
+
+openMainPage();
 
 document.querySelector('.login')?.addEventListener('click', () => {
   openSignIn();
+});
+
+document.querySelector('.logo-title')?.addEventListener('click', () => {
+  openMainPage();
 });
