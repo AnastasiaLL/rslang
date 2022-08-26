2ec19b5b7e1b79a371866512dce1885c384de0a7
AnastasiaLL <lukyanchik.nastya@bk.ru>
2022-08-26 18:56:20 +0300

    init commit

--- src/pages/stats/demoStatsPage.ts
+++ src/pages/stats/demoStatsPage.ts
@@ -1,40 +1,40 @@
-import { drawDoughnutChart, drawLineChart, drawBarChart } from './drawCharts';
-import { dayStats, newWordsByDayStats, studiedByDayStats } from './model/fakeStats';
-import { drawTodayStats, drawTotalStatsContainers, openStatsPage } from './openStatsPage';
-import Constants from '../../constants/Constants';
-import createBLock from '../../components/createBLock';
+// import { drawDoughnutChart, drawLineChart, drawBarChart } from './drawCharts';
+// import { dayStats, newWordsByDayStats, studiedByDayStats } from './model/fakeStats';
+// import { drawTodayStats, drawTotalStatsContainers, openStatsPage } from './openStatsPage';
+// import Constants from '../../constants/Constants';
+// import createBLock from '../../components/createBLock';
 
-export default function demonstrateStatsPage() {
-  const statsPage = createBLock('div', {
-    classList: ['stats'],
-  });
+// export default function demonstrateStatsPage() {
+//   const statsPage = createBLock('div', {
+//     classList: ['stats'],
+//   });
 
-  const mainBlock = document.querySelector('#main-block');
-  if (mainBlock) {
-    mainBlock.innerHTML = '';
-    mainBlock.append(statsPage);
-  }
+//   const mainBlock = document.querySelector('#main-block');
+//   if (mainBlock) {
+//     mainBlock.innerHTML = '';
+//     mainBlock.append(statsPage);
+//   }
 
-  const dayStatsData = dayStats;
-  const allTodayStats = drawTodayStats(dayStatsData);
-  const totalStats = drawTotalStatsContainers();
+//   const dayStatsData = dayStats;
+//   const allTodayStats = drawTodayStats(dayStatsData);
+//   const totalStats = drawTotalStatsContainers();
 
-  const demoButton = createBLock('button', {
-    classList: ['button', 'primary-button'],
-    children: [Constants.statisticPage.demoButtonBack],
-    event: 'click',
-    listener: openStatsPage,
-  });
+//   const demoButton = createBLock('button', {
+//     classList: ['button', 'primary-button'],
+//     children: [Constants.statisticPage.demoButtonBack],
+//     event: 'click',
+//     listener: openStatsPage,
+//   });
 
-  statsPage.append(allTodayStats, totalStats, demoButton);
+//   statsPage.append(allTodayStats, totalStats, demoButton);
 
-  dayStatsData.forEach((pieceOfData) => {
-    drawDoughnutChart(pieceOfData);
-  });
+//   dayStatsData.forEach((pieceOfData) => {
+//     drawDoughnutChart(pieceOfData);
+//   });
 
-  const newWordsByDayStatsData = newWordsByDayStats;
-  const studiedByDayStatsData = studiedByDayStats;
+//   const newWordsByDayStatsData = newWordsByDayStats;
+//   const studiedByDayStatsData = studiedByDayStats;
 
-  drawBarChart(newWordsByDayStatsData, 'words-by-day');
-  drawLineChart(studiedByDayStatsData, 'studied-by-day');
-}
+//   drawBarChart(newWordsByDayStatsData, 'words-by-day');
+//   drawLineChart(studiedByDayStatsData, 'studied-by-day');
+// }
